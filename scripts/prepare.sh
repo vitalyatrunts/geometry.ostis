46a22c7cb171a8d91743fded928de3e00303f926
ShunkevichDV <shu.dv@tut.by>
2015-05-20 10:10:19 +0300

    Dialog added

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -34,7 +34,7 @@ stage "Clone projects"
 clone_project https://github.com/deniskoronchik/sc-machine.git sc-machine master
 clone_project https://github.com/ShunkevichDV/scp-machine.git scp-machine master
 clone_project https://github.com/deniskoronchik/sc-web.git sc-web master
-clone_project https://github.com/deniskoronchik/ims.ostis.kb.git ims.ostis.kb master
+clone_project https://github.com/ShunkevichDV/ims.ostis.kb.git ims.ostis.kb master
 clone_project https://github.com/alexei-yasko/ostis.geometry.drawings.git geometry.drawings master
 
 stage "Prepare projects"
