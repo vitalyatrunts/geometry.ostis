ce81e72cc0bdf31d991c74e6f5355221ca8e3429
ShunkevichDV <shu.dv@tut.by>
2016-02-25 20:17:09 +0300

    Dev branch scripts added

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -33,7 +33,7 @@ stage "Clone projects"
 
 clone_project https://github.com/ShunkevichDV/sc-machine.git sc-machine master
 clone_project https://github.com/deniskoronchik/sc-web.git sc-web master
-clone_project https://github.com/ShunkevichDV/ims.ostis.kb.git ims.ostis.kb master
+clone_project https://github.com/ShunkevichDV/ims.ostis.kb.git ims.ostis.kb dev
 clone_project https://github.com/alexei-yasko/ostis.geometry.drawings.git geometry.drawings master
 
 stage "Prepare projects"
