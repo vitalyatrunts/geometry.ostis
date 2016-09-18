e4f62d145c688ede486af22b786eb05ec53bd7b2
DziubinaElena <lioness10003@mail.ru>
2016-09-18 13:27:46 +0300

    Update geometry.drawings repository URL (#132)

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -34,7 +34,7 @@ stage "Clone projects"
 clone_project https://github.com/deniskoronchik/sc-machine.git sc-machine master
 clone_project https://github.com/Ivan-Zhukau/sc-web.git sc-web master
 clone_project https://github.com/ShunkevichDV/ims.ostis.kb.git ims.ostis.kb master
-clone_project https://github.com/alexei-yasko/ostis.geometry.drawings.git geometry.drawings master
+clone_project https://github.com/MikhailSadovsky/geometry.drawings.git geometry.drawings master
 
 stage "Prepare projects"
 
